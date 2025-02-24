4efaaf8b8242dceb3b7a6d53646842b2373a6e80
wiizh92 <wiizh92@g.ucla.edu>
2025-02-23 20:37:02 -0800

    updated commit

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'eslint.config.js'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
@@ -33,6 +33,17 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+      // Disable prop-types validation.
+      "react/prop-types": 0,
     },
+    // Add an overrides array. Without this, ESLint could only be run via
+    // the npm run lint command. Adding this will allow it to be run also
+    // via the global `npx eslint .` command.
+    overrides: [
+      {
+      // Without this, `npx eslint .` doesn't run on jsx files.
+      files: ["*.js", "*.jsx"],
+      },
+    ],
   },
 ]
